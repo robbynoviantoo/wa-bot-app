60dc61b0d7d7ca0ab8c60b21722c318c35bfa5c3
robbynoviantoo <heyitsrobby99@gmail.com>
2025-04-15 19:40:05 +0700

    tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MenuPage from './pages/MenuPage';
+import './App.css'
 
 const App: React.FC = () => {
   return (
