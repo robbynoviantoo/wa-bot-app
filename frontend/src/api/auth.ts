d193a255cde6cbad625b57995b2d58d2e1023e4e
robbynoviantoo <heyitsrobby99@gmail.com>
2025-04-17 15:41:43 +0700

    done

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const login = async (username: string, password: string) => {
   const response = await axios.post(
-    "http://localhost:3009/api/auth/login",
+    "http://103.23.198.66:3009/api/auth/login",
     { username, password },
     {
       withCredentials: true,
